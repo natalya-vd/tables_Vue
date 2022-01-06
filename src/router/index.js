6fd3c753f31f1180920a194b1dae6abf693259e4
Natalya <natasha-vd@yandex.ru>
2022-01-07 01:54:48 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import BaseWrapper from '../components/base-table/index.vue';
+import PaginationWrapper from '../components/pagination/index.vue';
+import VirtalScrollWrapper from '../components/virtual-scroll/index.vue';
+import FunctionalWrapper from '../components/functional-components/index.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: BaseWrapper
+        },
+    
+        {
+            path: '/pagination',
+            name: 'pagination',
+            component: PaginationWrapper
+        },
+
+        {
+            path: '/virtual-scroll',
+            name: 'virtual-scroll',
+            component: VirtalScrollWrapper
+        },
+
+        {
+            path: '/functional',
+            name: 'functional',
+            component: FunctionalWrapper
+        }
+    ]
+});
